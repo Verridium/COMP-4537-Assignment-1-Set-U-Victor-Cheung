a52535b531e64d53d716c31ce79664b8372f7f3e
Verridium <77175850+Verridium@users.noreply.github.com>
2022-11-07 17:07:07 -0800

    Lab 8 Added

--- server.js
+++ server.js
@@ -1,74 +1,124 @@
 const express = require('express')
 const mongoose = require('mongoose');
-const { request } = require('node:http');
+const {connectDB} = require("./connectDB.js")
+const {populatePokemons} = require("./populatePokemons.js")
+const {getTypes} = require("./getTypes.js")
+const {handleErr} = require("./errorHandler.js")
+
+const {
+    PokemonBadRequest,
+    PokemonNotFoundError,
+    PokemonDbError,
+    PokemonDuplicateError,
+    PokemonBadRequestMissingID,
+    PokemonBadRequestMissingAfter,
+    PokemonNoSuchRouteError
+} = require('./error.js');
+
+const { asyncWrapper } = require('./asyncWrapper.js');
 
 const app = express()
 const port = 5000
+var pokeModel = null;
 
-const Schema = mongoose.Schema;
-    var possibleTypes = []
-    var pokeSchema = new Schema ({
-        "base": {
-            "HP": Number,
-            "Attack": Number,
-            "Defense": Number,
-            "Speed": Number,
-            "Sp. Attack": Number,
-            "Sp. Defense": Number,
-        },
-        "id": Number,
-        "name": {
-            "english": String,
-            "japanese": String,
-            "chinese": String,
-            "french": String
-        },
-        "type": [
-            String, 
-            String
-        ],
-        "__v": Number
+const start = asyncWrapper(async (req, res, next) => {
+    await connectDB();
+    const pokeSchema = await getTypes();
+    pokeModel = await populatePokemons(pokeSchema);
+    app.listen(port, () => {
+        if (err)
+            throw new PokemonDbError(err);
+        else
+            console.log(`phew! server is running on port: ${port}`);
     })
-
-    const pokeModel = mongoose.model('Pokemon', pokeSchema);
-
-    
-app.listen(process.env.PORT || port, async () => {
-    try {
-        await mongoose.connect('mongodb+srv://user1:VRJk55Ky8glhQoQe@cluster0.eg3xtej.mongodb.net/Pokemon?retryWrites=true&w=majority')
-        mongoose.connection.db.dropDatabase();
-    } catch (error) {
-        console.log('db error')
-    }
-    console.log(`Server running on port ${port}`)
-
-    var https = require('node:https');
-    //grab the types
-    await https.get("https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/types.json", async (res) => {
-        let data = "";
-        res.on("data", (chunk) => {
-            data += chunk;
-        });
-        res.on("end", async () => {
-            possibleTypes = JSON.parse(data);
-        }); 
-    })
-
-   //grab the pokemon
-    https.get("https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json", function(res) {
-        var chunks = "";
-        res.on("data", function(chunk) {
-            chunks += chunk;
-        });
-        res.on("end", function(data) {
-            const arr = JSON.parse(chunks);
-            arr.map(element => {
-                mongoose.connection.db.collection('pokemons').insertOne(element);
-            })
-        })
-    })
-
 })
+
+start();
+
+app.get('/api/v1/pokemons', asyncWrapper(async (req, res) => {
+    if (!req.query["count"])
+      req.query["count"] = 10
+    if (!req.query["after"])
+      req.query["after"] = 0
+    const docs = await pokeModel.find({})
+      .sort({ "id": 1 })
+      .skip(req.query["after"])
+      .limit(req.query["count"])
+    res.json(docs)
+  }))
+
+app.get('/api/v1/pokemon/:id', asyncWrapper(async (req, res) => {
+const { id } = req.params
+const docs = await pokeModel.find({ "id": id })
+if (docs.length != 0) res.json(docs)
+else res.json({ errMsg: "Pokemon not found" })
+}))
+
+app.use(express.json())
+
+app.post('/api/v1/pokemon/', asyncWrapper(async (req, res) => {
+  if (!req.body.id) throw new PokemonBadRequestMissingID()
+  const poke = await pokeModel.find({ "id": req.body.id })
+  if (poke.length != 0) throw new PokemonDuplicateError()
+  const pokeDoc = await pokeModel.create(req.body)
+  res.json({
+    msg: "Added Successfully"
+  })
+}))
+
+app.delete('/api/v1/pokemon/:id', asyncWrapper(async (req, res) => {
+    const docs = await pokeModel.findOneAndRemove({ id: req.params.id })
+    if (docs)
+      res.json({
+        msg: "Deleted Successfully"
+      })
+    else
+      throw new PokemonNotFoundError("");
+  }))
+
+app.put('/api/v1/pokemon/:id', asyncWrapper(async (req, res) => {
+const selection = { id: req.params.id }
+const update = req.body
+const options = {
+    new: true,
+    runValidators: true,
+    overwrite: true
+}
+const doc = await pokeModel.findOneAndUpdate(selection, update, options)
+if (doc) {
+    res.json({
+    msg: "Updated Successfully",
+    pokeInfo: doc
+    })
+} else {
+    throw new PokemonNotFoundError("");
+}
+}))
+
+app.patch('/api/v1/pokemon/:id', asyncWrapper(async (req, res) => {
+const selection = { id: req.params.id }
+const update = req.body
+const options = {
+    new: true,
+    runValidators: true
+}
+const doc = await pokeModel.findOneAndUpdate(selection, update, options)
+if (doc) {
+    res.json({
+    msg: "Updated Successfully",
+    pokeInfo: doc
+    })
+} else {
+    throw new PokemonNotFoundError("");
+}
+}))
+
+app.get("*", (req, res) => {
+throw new PokemonNoSuchRouteError("");
+})
+
+
+app.use(handleErr)
 
 
 app.get("/pokemonsAdvancedFiltering", async (req, res) => {
@@ -100,95 +150,19 @@ app.get("/pokemonsAdvancedFiltering", async (req, res) => {
         }
     })
 
-    const pokemons = await pokeModel.find(query);
+    const pokemons = await pokeModel.find(match);
     res.send(pokemons);
 
 })
 
-app.get('/api/v1/pokemon/:id', (req, res) => {
-    if(isNaN(req.params.id)){
-        res.json({errmsg: "CastError: Check your input"})
-    } else {
-    pokeModel.find({ id: req.params.id }, function(err, data) {
-        if (err) {
-            res.json({errmsg: "Pokemon not found"})
-        } 
-        else if (data.length <= 0) {
-            res.json({errmsg: "Pokemon not found"})
-        } 
-        else {
-            res.send(data)
-        }
-    })
-    }
-})
-
-// - get all the pokemons after the 10th. List only Two.
-app.get('/api/v1/pokemons', (req, res) => {
-    pokeModel.find({"id":{$gt:req.query.after}}, null, {limit:req.query.count}, function(err, data) {
-        if (err) {
-            res.json({errmsg: "invalid count or after"});
-        } else {
-            res.send(data);
-        }
-    });
-});
-
-// - create a new pokemon    
-app.use(express.json());
-app.post('/api/v1/pokemon', async (req, res) => {
-    var count = await pokeModel.countDocuments({id:req.params.id})
-    pokeModel.create(req.body, function(err, data) {
-        if(err) {
-            res.json({errmsg: "invalid pokemon"});
-        } else if(req.body.name.english.length > 25) {
-            res.json({errmsg: "ValidationError: check your name length."});
-        } else if(count == 1) { // can't find id in db
-            res.json({errmsg: "ValidationError: check your id."});
-        }
-        else {
-            res.send({msg: "Added Successfully"});
-            console.log(data);
-        }
- })                       
-})
-
-// - delete a pokemon
-app.delete('/api/v1/pokemon/:id', (req, res) => {
-    pokeModel.findOneAndDelete({id: req.params.id}, function(err, result) {
-        if(!result) {
-            res.json({errmsg: "Pokemon not found"});
-        } else {
-            console.log(result);
-            res.send("Deleted Successfully"); 
-        }
-    })
-})
-
-// - upsert a whole pokemon document
-app.put('/api/v1/pokemon/:id', (req, res) => {
-    pokeModel.findOneAndUpdate({id: req.params.id}, req.body, function(err, result) {
-        if(err) {
-            res.json({errmsg: "Pokemon not found"});
-        } else if (!result){
-            console.log(result);
-            res.send("Updated Successfully");
-        } else {
-        }
-    })
-})
-
-// - patch a pokemon document or a portion of the pokemon document
-app.patch('/api/v1/pokemon/:id', (req, res) => {
-    pokeModel.findOneandUpdateOne({id: req.params.id}, req.body, function(err, result) {
-        if(err) {
-            res.json({errmsg: "Pokemon not found"});
-        } else {
-            console.log(result);
-            res.send("Updated Successfully");
-        }
-    })
-})
+app.patch("/pokemonsAdvancedFiltering"), async (req, res) => {
+    const pushOperator = req.query.pushOperator
+    const filters = pushOperator.split(",").map((operator) => operator.trim());
+    const filterRegex = [];
+    
+    const pokemons = await pokeModel.find(query);
+    res.send(pokemons);
+}
 
 // - get a pokemon Image URL
 app.get('/api/v1/pokemonImage/:id', (req, res) => {
